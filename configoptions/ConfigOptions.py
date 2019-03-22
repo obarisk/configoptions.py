ae43ceab3f603935ca0651752c11248d217d13fa
obarisk <obarisk@gmail.com>
2019-03-22 16:13:39 +0000

    use safe loader

--- configoptions/ConfigOptions.py
+++ configoptions/ConfigOptions.py
@@ -4,7 +4,7 @@ import os
 import yaml
 
 
-class ConfigOptions:
+class ConfigOptions():
     def __init__(self,
                  val={},
                  cfg=["/etc/config", "./config", "./config.yml"]):
@@ -19,13 +19,13 @@ class ConfigOptions:
                     ymls += [x for x in os.listdir(PTH) if x.endswith(".yaml")]
                     for FTH in sorted(ymls):
                         with open("{0}/{1}".format(PTH, FTH)) as f:
-                            ndict = yaml.load(f)
+                            ndict = yaml.load(f, Loader=yaml.SafeLoader)
                         for k in ndict:
                             opts.update({k: ndict.get(k)})
                 elif (os.path.isfile(PTH) and
                         (PTH.endswith(".yml") or PTH.endswith(".yaml"))):
                     with open("{0}".format(PTH)) as f:
-                        ndict = yaml.load(f)
+                        ndict = yaml.load(f, Loader=yaml.SafeLoader)
                     for k in ndict:
                         opts.update({k: ndict.get(k)})
         if len(val):
